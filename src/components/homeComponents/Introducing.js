fb0f9171ae84130c8e51009ca4bf7ffcbf66e74c
HebaAli48 <131808003+HebaAli48@users.noreply.github.com>
2023-10-14 19:53:09 +0300

    edited in image size of introducing component

--- src/components/homeComponents/Introducing.js
+++ src/components/homeComponents/Introducing.js
@@ -4,17 +4,17 @@ const Introducing = () => {
   return (
     <>
       <div className="bg-teal-100 h-[75vh] flex flex-col   justify-center sm:flex-row  sm:justify-between ">
-        <div className="w-full sm:w-[50vw] h-80    sm:order-first order-last mt-40 sm:mt-0  mx-auto sm:py-10 relative  bottom-48 sm:bottom-0">
+        <div className="w-full sm:w-[90vw] h-80    sm:order-first order-last mt-52 sm:mt-20  mx-auto sm:py-10 relative  bottom-52 sm:bottom-0">
           <img
             src={shieldImg}
-            className="sm:absolute sm:inset-0  w-full h-[90%] sm:h-full sm:my-10 object-contain"
+            className="sm:absolute sm:inset-0  w-full md:w-[80%] h-full sm:my-10 object-center mx-auto"
             alt="introducingPic"
           />
         </div>
 
         <div className="flex flex-col items-end justify-center">
           <h2 className="bg-teal-300  text-white w-8 sm:w-fit  h-4/5 sm:px-2 flex items-center relative bottom-2 sm:bottom-24  z-20 pt-32 pb-32 rounded-bl-[40px]">
-            <span className="inline-block rotate-[-90deg] translate-y-[70px]  translate-x-[-8px] pb-5s sm:pb-0 pt-auto relative">
+            <span className="inline-block rotate-[-90deg] translate-y-[90px]  translate-x-[-8px]  sm:translate-y-0  sm:translate-x-0 pb-5s sm:pb-0 pt-auto relative">
               عـنـوان
             </span>
           </h2>
