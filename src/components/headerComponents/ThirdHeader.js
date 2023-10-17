01e4513682ab30e4ed274a19f182d11e0c52c91e
HebaAli48 <131808003+HebaAli48@users.noreply.github.com>
2023-10-17 15:49:56 +0300

    added hover color on lists of third header

--- src/components/headerComponents/ThirdHeader.js
+++ src/components/headerComponents/ThirdHeader.js
@@ -8,32 +8,32 @@ const ThirdHeader = () => {
     {
       id: 0,
       title: "سيدات",
-      color: "bg-red-700",
+      color: "hover:bg-red-700",
     },
     {
       id: 1,
       title: "كبار السن",
-      color: "bg-teal-700",
+      color: "hover:bg-teal-700",
     },
     {
       id: 2,
       title: "أباء ووصاه",
-      color: "bg-red-300",
+      color: "hover:bg-red-300",
     },
     {
       id: 3,
       title: "المعلمين",
-      color: "bg-amber-600",
+      color: "hover:bg-amber-600",
     },
     {
       id: 4,
       title: "شباب",
-      color: "bg-purple-700",
+      color: "hover:bg-purple-700",
     },
     {
       id: 5,
       title: "أطفال",
-      color: "bg-teal-400",
+      color: "hover:bg-teal-400",
     },
   ];
 
@@ -55,7 +55,7 @@ const ThirdHeader = () => {
             <ul
               className={` ${
                 isCollapsed
-                  ? "relative  h-[75vh]  w-full bg-slate-50 transition-all ease-in-out duration-500  shadow-md  rounded-b-2xl z-50 flex flex-col gap-5 px-5 mx-auto py-5  border-b-[1px]    m-auto text-right  items-end text-[8px]"
+                  ? "relative  h-[75vh]  w-full bg-slate-50 transition-all ease-in-out duration-500  shadow-md  rounded-b-2xl z-50 flex flex-col  gap-5 px-5 mx-auto py-5  border-b-[1px]    m-auto text-right  items-end text-[8px]"
                   : "hidden sm:flex flex-row flex-wrap text-center items-start sm:justify-center text-[8px] sm:text-sm sm:items-end w-4/5 m-auto"
               }`}
             >
@@ -64,9 +64,9 @@ const ThirdHeader = () => {
                   key={data.id}
                   className={` ${
                     isCollapsed
-                      ? "px-3 py-2 text-lg  font-medium hover:border-b-[1px] h-full w-full hover:scale-105"
+                      ? "px-3 pt-8 hover:shadow-md text-lg  font-medium hover:border-b-[1px] h-full w-full hover:scale-105"
                       : "px-3 py-2 w-32  rounded-b-2xl border shadow-md bg-white  hover:scale-125"
-                  } hover:${data.color}`}
+                  } ${data.color} hover:text-white`}
                 >
                   <NavLink to="/" className={`py-1 transition z-50`}>
                     {data.title}
